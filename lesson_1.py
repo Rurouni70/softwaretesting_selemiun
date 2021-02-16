19c9daf7860ddf5e40290c159e85133393f8395f
tsyupaigor <tsyupaigor@mail.com>
2021-02-16 21:38:46 +0700

    Solutions for the first task

---
+++ lesson_1.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_for_lesson(driver):
+    driver.get("https://software-testing.ru/")
+
