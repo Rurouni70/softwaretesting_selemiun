4fd49966f9e0dc2aa21b30a14af9a454152e7c96
tsyupaigor <tsyupaigor@mail.com>
2021-02-17 21:21:29 +0700

    Solutions for lesson 2

---
+++ lesson2_login_scenarios.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+link = 'http://localhost/litecart/admin/login.php'
+login = 'admin'
+password = 'admin'
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_for_lesson(driver):
+    driver.get(link)
+    driver.find_element_by_name("username").send_keys(login)
+    driver.find_element_by_name("password").send_keys(password)
+    driver.find_element_by_name("login").click()
